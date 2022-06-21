08c0823cadbe6609e899871ce1efe9ff69b6887f
Hong Shik Branden Kim <kimploo@gmail.com>
2022-06-21 20:02:16 +0900

    update script.js

--- script.js
+++ script.js
@@ -39,7 +39,6 @@ const convertToDiscussion = (obj) => {
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
-
 // agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링하는 함수입니다.
 const render = (element) => {
   for (let i = 0; i < agoraStatesDiscussions.length; i += 1) {
@@ -73,6 +72,7 @@ form.addEventListener("submit", (event) => {
   ul.prepend(discussion);
 });
 
+  // then => res => res.json() => json => render
 fetch('http://localhost:3001/discussions').then((res) => { 
   return res.json();
 }).then((json) => {
